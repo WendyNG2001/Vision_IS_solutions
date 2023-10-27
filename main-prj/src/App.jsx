b90157bb8cf6ad2cd62700935c62f075408ad7aa
nqoba-ndlovu <141147501+nqoba-ndlovu@users.noreply.github.com>
2023-10-27 10:24:28 +0000

    created records for collections

--- main-prj/src/App.jsx
+++ main-prj/src/App.jsx
@@ -1,13 +1,16 @@
 import { Admin, Resource, ListGuesser} from "react-admin";
 import { PocketBaseProvider } from "./ra-pocketbase";
-import { Stu_comp } from "./Stu_comp";
 
 
-const pbProvider = PocketBaseProvider('https://fantastic-adventure-4j7w7vvgw5rrf7x49-8090.app.github.dev/');
 
+const pbProvider = PocketBaseProvider('https://fantastic-adventure-4j7w7vvgw5rrf7x49-8090.app.github.dev/'); //use it to access  collections in PocketBase 
+//function returns the relevent code needed to create the collections as components
 function App() {
   return (
     <Admin dataProvider={pbProvider.dataProvider} >
+      <Resource name="Student" list={ListGuesser}/>
+      <Resource name="Student" list={ListGuesser}/>
+      <Resource name="Student" list={ListGuesser}/>
       <Resource name="Student" list={ListGuesser}/>
     </Admin>
   )
--- main-prj/src/main.jsx
+++ main-prj/src/main.jsx
@@ -1,7 +1,8 @@
+// import the relevant methods and operations needed to successfully import and run  the App component in this file
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-//import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
